36a1004e8646a70cfd29a63421d7eb852bae77f5
Dmitry Ponyatov <dponyatov@gmail.com>
2019-03-04 23:04:45 +0400

    + FORTH REPL
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hico.py
+++ hico.py
@@ -1,4 +1,6 @@
 
+import sys
+
 class Frame:
     def __init__(self,V):
         self.type  = self.__class__.__name__.lower()
@@ -7,8 +9,12 @@ class Frame:
         self.nest  = []
     def __repr__(self):
         return self.dump()
+    dumped = []
     def dump(self,depth=0,prefix=''):
         S = self.pad(depth) + self.head(prefix)
+        if not depth: Frame.dumped = []
+        if self in Frame.dumped: return S + ' ...'
+        else: Frame.dumped.append(self)
         for i in self.attr: S += self.attr[i].dump(depth+1,prefix='%s = '%i)
         for j in self.nest: S += j.dump(depth+1)
         return S
@@ -21,21 +27,52 @@ class Frame:
         if isinstance(obj,str): self.nest.append(String(obj))
         else: self.nest.append(obj)
         return self
+    def __lshift__(self,obj):
+        self.attr[obj.value] = obj ; return self
+    def __getitem__(self,slot):
+        return self.attr[slot]
+    def __setitem__(self,slot,obj):
+        self.attr[slot] = obj ; return self
+        
+    def top(self): return self.nest[-1]
+    def pop(self): return self.nest.pop()
+    def dropall(self): self.nest = []
     
     def gen(self):
         S = ''
         for i in self.nest: S += i.gen() + '\n'
         return S
         
-class Primitive(Frame): pass
-
-class String(Primitive):
+class Primitive(Frame):
     def gen(self):
         return self.value
+
+class Symbol(Primitive): pass
+
+class String(Primitive): pass
     
 class Container(Frame): pass
 
 class Group(Container): pass
+
+class Stack(Container): pass
+
+class Dict(Container):
+    def __setitem__(self,slot,obj):
+        if callable(obj): self[slot] = VM(obj) ; return self
+        else: return Container.__setitem__(self, slot, obj)
+    def __lshift__(self,obj):
+        if callable(obj): self << VM(obj) ; return self
+        else: return Container.__lshift__(self, obj)
+        
+class Active(Frame): pass
+
+class VM(Active):
+    def __init__(self,F):
+        Active.__init__(self,F.__name__)
+        self.fn = F
+    def execute(self):
+        self.fn()
     
 class IO(Frame): pass
     
@@ -74,7 +111,54 @@ e_project // '''<?xml version="1.0" encoding="UTF-8"?>
     </natures>
 </projectDescription>'''
 
-print hico.gen()
+import ply.lex as lex
 
-# print e_project.gen()
+tokens = ['symbol']
 
+t_ignore = ' \t\r\n'
+
+def t_symbol(t):
+    r'[a-zA-Z0-9_.]+'
+    return Symbol(t.value)
+
+def t_error(t): return SyntaxError(t)
+
+lexer = lex.lex()
+
+S = Stack('DATA')
+W = Dict('WORDS') ; W['W'] = W
+
+def DROPALL(): S.dropall()
+W['.'] = DROPALL
+
+def WORD():
+    token = lexer.token()
+    if not token: return False
+    S // token ; return True
+W << WORD
+    
+def FIND():
+    T = S.pop()
+    try: S // W[T.value] ; return True
+    except KeyError: S // T ; return False
+W << FIND
+    
+def EXECUTE():
+    S.pop().execute()
+W << EXECUTE
+    
+def INTERPRET():
+    lexer.input(S.pop().value)
+    while True:
+        if not WORD(): break;
+        if isinstance(S.top(),Symbol):
+            if FIND(): EXECUTE()
+W << INTERPRET
+
+def REPL():
+    while True:
+        print W ; print S ; print
+        try: S // String(raw_input('hico> '))
+        except EOFError: sys.exit()
+        INTERPRET()
+REPL()
