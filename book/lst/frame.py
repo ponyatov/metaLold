9425472b9e3c23b2b4a206f10061b9c6b99f8c3d
Dmitry Ponyatov <dponiatov@icbcom.ru>
2019-03-05 12:26:00 +0400

    + book/listings
    Signed-off-by: Dmitry Ponyatov <dponiatov@icbcom.ru>

---
+++ book/lst/frame.py
@@ -0,0 +1,16 @@
+# extended frame model [Marvin Minsky]
+class Frame:
+    # construct frame with given name
+    def __init__(self,V):
+        # class/type tag (string for simplicity)
+        self.type  = self.__class__.__name__.lower()
+        # atomic value (Python primitive type)
+        self.value = V
+        # slots/attributes /associative array/
+        self.attr  = {}
+        # nested objects /ordered list = stack/
+        self.nest  = []
+ 
+print Frame('hello')
+
+<frame:hello>
