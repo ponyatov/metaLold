3e43bfeb778da4856e98a45e651da4921e7ff71c
Dmitry Ponyatov <dponyatov@gmail.com>
2019-03-10 11:39:31 +0400

    + minimial pyF
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- book/forth/words.py
+++ book/forth/words.py
@@ -1,1 +1,4 @@
 W = Dict('WORDS') ; W['W'] = W
+
+<dict:WORDS> @7f97d0573098
+    W = <dict:WORDS> @7f97d0573098 ...
--- book/frames/dict.py
+++ book/frames/dict.py
@@ -1,1 +1,15 @@
-class Dict(Container): pass
+class Dict(Container):
+    
+  # operator dict[slot]=obj
+  def __setitem__(self,slot,obj):
+    # if function?
+    if callable(obj):
+      self[slot] = VM(obj) ; return self
+    else: return Container.__setitem__(self, slot, obj)
+    
+  # operator dict << obj
+  def __lshift__(self,obj):
+    # if function?
+    if callable(obj):
+      self << VM(obj) ; return self
+    else: return Container.__lshift__(self, obj)
