3e61491373adadece827f82dae64a097ab885a22
Dmitry Ponyatov <dponyatov@gmail.com>
2019-03-29 14:04:57 +0400

    + landscape activity
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- Android/app/src/main/java/io/github/ponyatov/metal/MainActivity.java
+++ Android/app/src/main/java/io/github/ponyatov/metal/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-                pad.setText("'new task' Task\n"+pad.getText());
+                pad.setText("'newTask' Task\n"+pad.getText());
             }
         });
     }
---
+++ Android/app/src/main/java/io/github/ponyatov/metal/MainLandActivity.java
@@ -0,0 +1,13 @@
+package io.github.ponyatov.metal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainLandActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_land);
+    }
+}
