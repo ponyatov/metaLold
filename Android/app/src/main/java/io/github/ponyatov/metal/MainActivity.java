0aef95332a26c41d59a8874c4256985f1f2cb8cd
Dmitry Ponyatov <dponyatov@gmail.com>
2019-03-29 13:37:30 +0400

    + [+] put task in pad
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- Android/app/src/main/java/io/github/ponyatov/metal/MainActivity.java
+++ Android/app/src/main/java/io/github/ponyatov/metal/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +19,15 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final TextView pad = findViewById(R.id.pad);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                pad.setText("'new task' Task\n"+pad.getText());
             }
         });
     }
