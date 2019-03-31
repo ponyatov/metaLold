2a755ade70064822e44829d8308ba7a59a02cd23
Dmitry Ponyatov <dponyatov@gmail.com>
2019-03-31 15:20:16 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- Android/app/src/main/java/io/github/ponyatov/metal/MainActivity.java
+++ Android/app/src/main/java/io/github/ponyatov/metal/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -47,11 +48,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
                 pad.setText("'newTask' Task\n"+pad.getText());
             }
         });
+
     }
 
     @Override
