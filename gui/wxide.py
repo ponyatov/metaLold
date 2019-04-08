3c46b1011446f710ea70a0f202580b0a38295766
Dmitry Ponyatov <dponyatov@gmail.com>
2019-04-08 14:24:21 +0400

    + gui/filename
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- gui/wxide.py
+++ gui/wxide.py
@@ -1,17 +1,37 @@
-import sys,wx
+import sys,wx,wx.stc
 
 ide = wx.App()
 
 class ideWindow(wx.Frame):
-    def __init__(self,V):
-        wx.Frame.__init__(self,parent=None,title=V)
+    def __init__(self,FileName):
+        wx.Frame.__init__(self,parent=None,title=FileName)
+        self.filename = FileName
         self.initMenu()
+        self.initEditor()
+        
+    def initEditor(self):
+        self.editor = wx.stc.StyledTextCtrl(self)
+        self.onLoad()
+        
+    def onLoad(self,event=None):
+        try:
+            with open(self.filename) as F:
+                self.editor.SetValue(F.read()) ; F.close()
+        except IOError: # no file
+            self.editor.SetValue('# %s\n\n' % self.filename)
+            self.onSave()
+            
+    def onSave(self,event=None):
+        with open(self.filename,'w') as F:
+            F.write( self.editor.GetValue() ) ; F.close()
+        
     def initMenu(self):
         self.menu = wx.MenuBar() ; self.SetMenuBar(self.menu)
         
         self.file = wx.Menu() ; self.menu.Append(self.file,'&File')
-        self.save = self.file.Append(wx.ID_SAVE,'&Save')
-        self.quit = self.file.Append(wx.ID_EXIT,'&Quit')
+        self.save = self.file.Append(wx.ID_SAVE,'&Save\tCtrl+S')
+        self.Bind(wx.EVT_MENU,self.onSave,self.save)
+        self.quit = self.file.Append(wx.ID_EXIT,'&Quit\tCtrl+Q')
         self.Bind(wx.EVT_MENU,self.onQuit,self.quit)
         
         self.debug = wx.Menu() ; self.menu.Append(self.debug,'&Debug')
@@ -21,8 +41,14 @@ class ideWindow(wx.Frame):
         self.about = self.help.Append(wx.ID_ABOUT,'&About\tF1')
         
     def onQuit(self,event):
+        ideConsole.onSave()
         ideConsole.Close()
 
-ideConsole = ideWindow(sys.argv[0]) ; ideConsole.Show()
+try:
+    autoloadFile = sys.argv[1]
+except:
+    autoloadFile = 'wxide.src'
+    
+ideConsole = ideWindow(autoloadFile) ; ideConsole.Show()
 
 ide.MainLoop()
