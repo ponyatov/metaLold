7aa8216baafff1afafde5d6246a8ee631b1b517e
Dmitry Ponyatov <dponyatov@gmail.com>
2019-04-11 02:42:27 +0400

    + stack/words
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ book/gui/wx08.py
@@ -0,0 +1,9 @@
+class ideWindow(wx.Frame):
+    def onQuit(self,event):
+        ideConsole.onSave() ; ideConsole.Close()
+        ideStack.Close() ; ideWords.Close()
+
+ideStack = ideWindow(autoloadFile+'.stack')
+ideStack.Show()
+ideWords = ideWindow(autoloadFile+'.words')
+ideWords.Show()
