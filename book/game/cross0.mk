447531beaeaebff70d1672ffd9fb0bf07d956ab4
Dmitry Ponyatov <dponyatov@gmail.com>
2019-04-16 11:55:53 +0400

    + build game/cross0
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- book/game/cross0.mk
+++ book/game/cross0.mk
@@ -1,9 +1,9 @@
 CPU     = i386
-CFG_CPU = --with-cpu=$(CPU) --with-float=soft
 TARGET  = $(CPU)-elf
+CFG_CPU = --with-cpu=$(CPU) --with-float=soft
 CROSS   = $(CURDIR)/$(TARGET)/bin
+QEMU	= qemu-system-i386
 
 cross: ../mcu/Makefile
 	make -f $< \
-		TARGET=$(TARGET) CFG_CPU="$(CFG_CPU)" \
-			dirs gz cclibs binutils gcc0
+		TARGET=$(TARGET) CFG_CPU="$(CFG_CPU)" cross0
