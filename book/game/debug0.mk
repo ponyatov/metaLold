ec78fd5bd41e404e1a039b6fd3a90096b649d441
Dmitry Ponyatov <dponyatov@gmail.com>
2019-04-16 17:04:09 +0400

    + ddd
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- book/game/debug0.mk
+++ book/game/debug0.mk
@@ -1,6 +1,8 @@
 QEMU	= qemu-system-i386
 GDB		= $(CROSS)/$(TARGET)-gdb
+CFLAGS += -g2
 
 go: game.elf
+	# exit from QEMU: [Ctrl-A] [X]
 	$(QEMU) -kernel $< -nographic -s -S &
-	$(GDB) -x $<.gdb $<
+	$(GDB) -x $<.gdb $< ; killall $(QEMU)
