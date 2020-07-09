b8caef0ca9b71a96a41a5576e78e1bd555822fc2
Dmitry Ponyatov <ponyatov@meteo.local>
2020-07-09 09:46:13 +0400

    Signed-off-by: Dmitry Ponyatov <ponyatov@meteo.local>

--- appengine_config.py
+++
@@ -1,3 +0,0 @@
-from google.appengine.ext import vendor
-# add any libraries installed in lib/ folder
-vendor.add('lib')
--- metaL.ml
+++
@@ -1,8 +0,0 @@
-# metaL: homoiconic metaprogramming language /metacircular model/  
-\ (c) Dmitry Ponyatov <dponyatov@gmail.com> CC BY-NC-ND
-
-\ FORTH line comments can be used
-
-\ number literals test
-
--01 +02.30 -4e+5 0xDeadBeef 0b1101 ? . ?
